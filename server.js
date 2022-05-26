db2dcd9c1922730cf348a2c495778689b1012c70
dev.heeseok <dev.heeseok@gmail.com>
2022-05-27 00:02:43 +0900

    refactor: server signup tutorial 정리 #13
    signup tutorial 코드 정리

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ const cors = require('cors');
 
 dotenv.config();
 
-mongoose.connect(process.env.DATABASE_ACCESS, () => console.log("Database connected"));
+mongoose.connect(process.env.DATABASE_ACCESS, () => console.log('Database connected'));
 
 app.use(express.json());
 app.use(cors());
-app.use('/app', routesUrls);
-app.listen(4000, () => console.log("server is up and running"));
+app.use('/user', routesUrls);
+
+app.listen(4000, () => console.log('Server is up and running'));
